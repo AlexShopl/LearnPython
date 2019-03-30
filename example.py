3acfcd24071906260c86f7db5a01bb428ef4bce8
AlexShopl <shapovalovlex007@gmail.com>
2019-03-30 11:52:06 +0300

    example fix str

--- example.py
+++ example.py
@@ -1,6 +1,23 @@
+"""
 print("Hello world!")
 
 # Comment
+
+for i in range(4):
+    print(i)
+"""
+"""
+year = input("Vvedite god ")
+year = int(year)
+is_leap = year % 4 == 0 and (year % 100 !=0 or year % 400 ==0)
+print(is_leap)
+"""
+
+str = "Hello"
+print(str*99)
+
+
+
 
 
 """
