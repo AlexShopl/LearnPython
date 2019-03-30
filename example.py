2fb3970163ae94557d024e8b3e6862a735147294
AlexShopl <shapovalovlex007@gmail.com>
2019-03-30 12:09:02 +0300

    all lesson 30-03-2019

--- example.py
+++ example.py
@@ -12,19 +12,44 @@ year = int(year)
 is_leap = year % 4 == 0 and (year % 100 !=0 or year % 400 ==0)
 print(is_leap)
 """
-
+"""
 str = "Hello"
 print(str*99)
+"""
+
+#if-elseif-else
+
+"""
+company = "google.com"
+if "my" in company:
+    print("Подстрока не найдена")
+elif "google" in company:
+    print("Подстока google найдена")
+else:
+    print("Подстрока не найдена")
+
+"""
+res=0
+for i in range(101):
+    res += i #res=res+1
+
+print(res)
+
+
+
+
 
 
 
 
 
 """
+
 
 dfgfdgfdgfd
 dfgfdgfdg
 dfgfdgfd
 
 """
+
 
