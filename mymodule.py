c33443be913c0013c10e744fb56e32456e5fb7d4
AlexShopl <shapovalovlex007@gmail.com>
2019-04-13 11:54:26 +0300

    create mypackage

--- mymodule.py
+++ mymodule.py
@@ -1,6 +1,11 @@
-import mypackage
+#import mypackage.utils
+from mypackage.utils import *
 
 if __name__ == "__main__":
     print("hello")
+
+print(__name__)
+#print(mypackage.utils.multiply(4,5))
+print(multiply(3,7))
 
 #print(mypackage)
