f73ff63cabe21d5be89b2fa5104a0e4883730a3d
AlexShopl <shapovalovlex007@gmail.com>
2019-04-13 09:48:11 +0300

    fix randomnumber add print

--- randomnumber.py
+++ randomnumber.py
@@ -1,6 +1,10 @@
 import random
+
 N=random.randint(1,100)   
-userGuess=-1        
+userGuess=-1
+
+print("Привет, давай поиграем в угадай число. Я загадываю число от 1 до 100.")
+
 while userGuess!=N:      
     userGuess=int(input("Угадай число от 1 до 100: "))      
     if userGuess > N:        
